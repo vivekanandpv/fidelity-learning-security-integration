eba06a28652155140d955746f6c1ab01beb62fab
Vivekanand Pujar <vivekanandpv@gmail.com>
2025-10-30 17:14:40 +0530

    done

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { Rest } from '../rest';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
+  rest = inject(Rest);
 
+  getCart() {
+    this.rest.makeGet('https://dummyjson.com/cart').subscribe({
+      next: (r) => console.log('Cart Response', r),
+    });
+  }
+
+  getProducts() {
+    this.rest.makeGet('https://dummyjson.com/products').subscribe({
+      next: (r) => console.log('Products Response', r),
+    });
+  }
 }
---
+++ src/app/token-interceptor.ts
@@ -0,0 +1,19 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { User } from './user';
+
+export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
+  const userService = inject(User);
+
+  if (userService.token()) {
+    const requestWithToken = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${userService.token()}`,
+      },
+    });
+
+    return next(requestWithToken);
+  } else {
+    return next(req);
+  }
+};
