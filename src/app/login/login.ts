ba7e3e7ba8b4da3b830a0b0d50cd458f02368724
Vivekanand Pujar <vivekanandpv@gmail.com>
2025-10-30 16:36:46 +0530

    done

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,11 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroupDirective,
+  NgForm,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { ErrorStateMatcher } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, ReactiveFormsModule, MatButtonModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
+  fb = inject(FormBuilder);
+  matcher = new MyErrorStateMatcher();
 
+  loginForm = this.fb.group({
+    username: ['', [Validators.required]],
+    password: ['', [Validators.required]],
+  });
+
+  get username(): FormControl {
+    return this.loginForm.controls['username'];
+  }
+
+  get password(): FormControl {
+    return this.loginForm.controls['password'];
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('form submitted', this.loginForm.value);
+    } else {
+      console.log('Cannot submit an invalid form');
+    }
+  }
 }
